0b32014ee49eaa4bd5508d858af31203c164b90a
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2022-06-21 10:34:28 +0100

    Récupération depuis un bouton radio

--- src/application/SampleController.java
+++ src/application/SampleController.java
@@ -6,8 +6,10 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 
 
 public class SampleController {
@@ -28,8 +30,12 @@ public class SampleController {
 	private TextArea txtCommentaire;
 	
 	@FXML
+	private ToggleGroup profil;
+	
+	@FXML
 	private void btnInscriptionHandler(Event e) {
 		//System.out.println("Bouton Senregistrer clicked...");
+		/*
 		String nom = txtNom.getText();  // récupérer le nom
 		String email = txtEmail.getText(); // récupérer l'email
 		String phone = txtPhone.getText(); // récupérer le phone
@@ -43,6 +49,9 @@ public class SampleController {
 		alert.setHeaderText("Bienvenue à ISIKA");
 		alert.setContentText("Félicitation, la session demarre en Septembre 2022");
 		alert.showAndWait();
-		/// fin partie alert
+		/// fin partie alert*/
+		
+		RadioButton temp = (RadioButton) profil.getSelectedToggle();
+		System.out.println(temp.getText());
 	}
 }
