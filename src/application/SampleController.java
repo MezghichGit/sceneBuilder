a04e40d457ddf7030ab4f4c92799c71f80c0239d
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2022-06-21 09:58:21 +0100

    fin part 1 de lexo 1

--- src/application/SampleController.java
+++ src/application/SampleController.java
@@ -4,6 +4,7 @@ package application;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 
@@ -20,12 +21,18 @@ public class SampleController {
 	
 	@FXML
 	private TextField txtPhone;
+	
+	@FXML
+	private TextArea txtCommentaire;
+	
 	@FXML
 	private void btnInscriptionHandler(Event e) {
 		//System.out.println("Bouton Senregistrer clicked...");
 		String nom = txtNom.getText();  // récupérer le nom
 		String email = txtEmail.getText(); // récupérer l'email
 		String phone = txtPhone.getText(); // récupérer le phone
+		String comment = txtCommentaire.getText();
 		System.out.println("Vous êtes : "+nom +" Email : "+ email + " Phone : "+phone);
+		System.out.println("Votre commentaire : "+comment);
 	}
 }
