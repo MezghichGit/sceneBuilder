9f4974b466989dad42d85e9cf2ef24a487e0486b
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2022-06-21 09:27:40 +0100

    recuperation des champs nom email et phone

--- src/application/SampleController.java
+++ src/application/SampleController.java
@@ -9,7 +9,7 @@ import javafx.scene.control.TextField;
 
 public class SampleController {
 	
-	@FXML
+	@FXML     // @FXML = C'est une annotation, c'est une méta-donnée sur un objet
 	private Button btnInscription;
 	
 	@FXML
@@ -19,8 +19,13 @@ public class SampleController {
 	private TextField txtEmail;
 	
 	@FXML
+	private TextField txtPhone;
+	@FXML
 	private void btnInscriptionHandler(Event e) {
-		System.out.println("Bouton Senregistrer clicked...");
-		
+		//System.out.println("Bouton Senregistrer clicked...");
+		String nom = txtNom.getText();  // récupérer le nom
+		String email = txtEmail.getText(); // récupérer l'email
+		String phone = txtPhone.getText(); // récupérer le phone
+		System.out.println("Vous êtes : "+nom +" Email : "+ email + " Phone : "+phone);
 	}
 }
