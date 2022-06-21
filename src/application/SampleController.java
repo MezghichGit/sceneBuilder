01404cf13c8ed1e7785009871510ff74354dd60f
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2022-06-21 09:05:46 +0100

    partie MVC

--- src/application/SampleController.java
+++ src/application/SampleController.java
@@ -1,5 +1,26 @@
 package application;
 
+
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+
 public class SampleController {
 	
+	@FXML
+	private Button btnInscription;
+	
+	@FXML
+	private TextField txtNom;
+	
+	@FXML
+	private TextField txtEmail;
+	
+	@FXML
+	private void btnInscriptionHandler(Event e) {
+		System.out.println("Bouton Senregistrer clicked...");
+		
+	}
 }
--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,7 @@
 module SceneBuilder_p1 {
 	requires javafx.controls;
 	requires javafx.fxml;
+	requires javafx.base;
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
