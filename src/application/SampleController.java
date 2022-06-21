71cc809e98933331ab48cb1bbb79478fa209836e
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2022-06-21 10:01:43 +0100

    exemple d'une alerte

--- src/application/SampleController.java
+++ src/application/SampleController.java
@@ -3,6 +3,8 @@ package application;
 
 import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
@@ -34,5 +36,13 @@ public class SampleController {
 		String comment = txtCommentaire.getText();
 		System.out.println("Vous êtes : "+nom +" Email : "+ email + " Phone : "+phone);
 		System.out.println("Votre commentaire : "+comment);
+		
+		/// début partie alert
+		Alert alert = new Alert(AlertType.INFORMATION);
+		alert.setTitle("Inscription avec succès");
+		alert.setHeaderText("Bienvenue à ISIKA");
+		alert.setContentText("Félicitation, la session demarre en Septembre 2022");
+		alert.showAndWait();
+		/// fin partie alert
 	}
 }
